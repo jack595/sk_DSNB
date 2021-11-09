db863af290fc3ac22cd84e13777388f721d3b25a
luoxj <luoxj@ihep.ac.cn>
2021-11-09 15:11:21 +0800

    finally realize loading EDM in python

--- root_tool/python_script/LoadDetsimEDM.py
+++ root_tool/python_script/LoadDetsimEDM.py
@@ -4,6 +4,8 @@
 # @Email: luoxj@ihep.ac.cn
 # @File: LoadDetsimEDM.py
 # import matplotlib.pylab as plt
+import os.path
+
 import numpy as np
 # plt.style.use("/afs/ihep.ac.cn/users/l/luoxj/Style/Paper.mplstyle")
 
@@ -12,9 +14,29 @@ import sys
 sys.path.append("/afs/ihep.ac.cn/users/l/luoxj/root_tool/python_script/")
 
 import ROOT
+
+# v_path = np.loadtxt("./path_lib.txt",dtype=str)
+# set_path = set()
+# for path in v_path:
+#     set_path.add(os.path.dirname(path))
+# for path in set_path:
+#     print(path)
+#     ROOT.gSystem.AddDynamicPath(path)
+
+# ROOT.gSystem.AddDynamicPath("/cvmfs/juno.ihep.ac.cn/centos7_amd64_gcc830/Pre-Release/J21v1r0-Pre2/offline/InstallArea/amd64_linux26/lib")
+# ROOT.gSystem.AddDynamicPath("/cvmfs/juno.ihep.ac.cn/centos7_amd64_gcc830/Pre-Release/J21v1r0-Pre2/offline/InstallArea/python/")
+# ROOT.gSystem.AddDynamicPath("/cvmfs/juno.ihep.ac.cn/centos7_amd64_gcc830/Pre-Release/J21v1r0-Pre2/mt.sniper/InstallArea/python/")
+# ROOT.gSystem.AddDynamicPath("/cvmfs/juno.ihep.ac.cn/centos7_amd64_gcc830/Pre-Release/J21v1r0-Pre2/sniper/InstallArea/python/")
+# ROOT.gSystem.AddDynamicPath("/cvmfs/juno.ihep.ac.cn/centos7_amd64_gcc830/Pre-Release/J21v1r0-Pre2/offline/InstallArea/amd64_linux26/lib/")
+# ROOT.gSystem.AddDynamicPath("/cvmfs/juno.ihep.ac.cn/centos7_amd64_gcc830/Pre-Release/J21v1r0-Pre2/mt.sniper/InstallArea/amd64_linux26/lib/")
+# ROOT.gSystem.AddDynamicPath("/cvmfs/juno.ihep.ac.cn/centos7_amd64_gcc830/Pre-Release/J21v1r0-Pre2/sniper/InstallArea/amd64_linux26/lib/")
+# ROOT.gSystem.AddDynamicPath("/cvmfs/juno.ihep.ac.cn/centos7_amd64_gcc830/Pre-Release/J21v1r0-Pre2/ExternalLibs/ROOT/6.22.08/lib/")
+# ROOT.gSystem.AddDynamicPath("/cvmfs/juno.ihep.ac.cn/centos7_amd64_gcc830/Pre-Release/J21v1r0-Pre2/ExternalLibs/CLHEP/2.4.1.0/lib/")
+# ROOT.gSystem.AddDynamicPath("/cvmfs/juno.ihep.ac.cn/centos7_amd64_gcc830/Pre-Release/J21v1r0-Pre2/offline/DataModel/EvtNavigator/amd64_linux26/")
+# print(ROOT.gSystem.ListLibraries())
+
 ROOT.gSystem.Load("libEDMUtil")
 ROOT.gSystem.Load("libSimEventV2")
-# ROOT.gSystem.Load("libSimHeaderV2")
 f = ROOT.TFile.Open("root://junoeos01.ihep.ac.cn//eos/juno/user/luoxj/PSD_LowE/alpha/detsim/root/detsim-992.root")
 tree = f.Get("Event/Sim/SimEvent")
 print(tree.GetEntries())
